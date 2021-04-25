ba1e5b3f3d2bd1febd608a80c0bb8d405b7dd500
Deepak Goswami <deepakgoswami@quantumphinance.com>
2021-04-25 18:52:42 +0530

    added tesla_car_wheels

--- src/container/teslaBattery.js
+++ src/container/teslaBattery.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import './teslaBattery.css';
 import TeslaNotice from '../component/teslaNotice/teslaNotice';
+import TeslaCar from '../component/TeslaCar/TeslaCar';
 
 class teslaBattery extends Component {
     render() {
         return(
             <form className="tesla-battery">
                 <h1>Range Per Charge</h1>
+                <TeslaCar/>
                 <TeslaNotice/>
             </form>
         )
